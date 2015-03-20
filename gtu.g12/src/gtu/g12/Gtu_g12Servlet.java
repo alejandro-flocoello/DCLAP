8c45c7ca2763c321ddbe894a86c09047cdc55b23
Daniel <daniel.gordo.tevar@alumnos.upm.es>
2015-03-20 10:52:29 +0100

    Primera versión, solo tiene constructores (probando la subida)

---
+++ gtu.g12/src/gtu/g12/Gtu_g12Servlet.java
@@ -0,0 +1,13 @@
+package gtu.g12;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Gtu_g12Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
---
+++ gtu.g12/src/gtu/g12/model/Solicitud.java
@@ -0,0 +1,218 @@
+package gtu.g12.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Solicitud implements Serializable {
+		private static final long serialVersionUID = 1L;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		
+		private Long id;
+		
+		private String nombre;
+		private String apellido1;
+		private String apellido2;
+		private String tipoDoc;
+		private String codDoc;
+		private String nacionalidad;
+		private String domicilio;
+		//private String fotografia;
+		private String nomUniv;
+		private String centroUniv;
+		private String correoUniv;
+		private String password;
+		private String categoria;
+		private int expediente;
+		private boolean monedero;
+		
+		private int cuentaBan;
+		private int pin;
+		private int cv2;
+		
+		//private String fotoFondo;
+		private int numTarjeta;
+		
+		private String estado;
+		
+		public Solicitud(String nombre,String apellido1,String apellido2,String tipoDoc,String codDoc,
+				String nacionalidad,String domicilio,String nomUniv,String centroUniv,String correoUniv,
+				String password,String categoria,int expediente) {
+			this.setNombre(nombre);
+			this.setApellido1(apellido1);
+			this.setApellido2(apellido2);
+			this.setTipoDoc(tipoDoc);
+			this.setCodDoc(codDoc);
+			this.setNacionalidad(nacionalidad);
+			this.setDomicilio(domicilio);
+			this.setNomUniv(nomUniv);
+			this.setCentroUniv(centroUniv);
+			this.setCorreoUniv(correoUniv);
+			this.setPassword(password);
+			this.setCategoria(categoria);
+			this.setExpediente(expediente);
+			
+			setMonedero(false);
+			setCuentaBan(0);
+			setPin(0);
+			setCv2(0);
+			setNumTarjeta(0);
+			setEstado("NADA");
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getApellido1() {
+			return apellido1;
+		}
+
+		public void setApellido1(String apellido1) {
+			this.apellido1 = apellido1;
+		}
+
+		public String getApellido2() {
+			return apellido2;
+		}
+
+		public void setApellido2(String apellido2) {
+			this.apellido2 = apellido2;
+		}
+
+		public String getTipoDoc() {
+			return tipoDoc;
+		}
+
+		public void setTipoDoc(String tipoDoc) {
+			this.tipoDoc = tipoDoc;
+		}
+
+		public String getCodDoc() {
+			return codDoc;
+		}
+
+		public void setCodDoc(String codDoc) {
+			this.codDoc = codDoc;
+		}
+
+		public String getNacionalidad() {
+			return nacionalidad;
+		}
+
+		public void setNacionalidad(String nacionalidad) {
+			this.nacionalidad = nacionalidad;
+		}
+
+		public String getDomicilio() {
+			return domicilio;
+		}
+
+		public void setDomicilio(String domicilio) {
+			this.domicilio = domicilio;
+		}
+
+		public String getNomUniv() {
+			return nomUniv;
+		}
+
+		public void setNomUniv(String nomUniv) {
+			this.nomUniv = nomUniv;
+		}
+
+		public String getCentroUniv() {
+			return centroUniv;
+		}
+
+		public void setCentroUniv(String centroUniv) {
+			this.centroUniv = centroUniv;
+		}
+
+		public String getCorreoUniv() {
+			return correoUniv;
+		}
+
+		public void setCorreoUniv(String correoUniv) {
+			this.correoUniv = correoUniv;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public int getExpediente() {
+			return expediente;
+		}
+
+		public void setExpediente(int expediente) {
+			this.expediente = expediente;
+		}
+
+		public String getCategoria() {
+			return categoria;
+		}
+
+		public void setCategoria(String categoria) {
+			this.categoria = categoria;
+		}
+
+		public boolean isMonedero() {
+			return monedero;
+		}
+
+		public void setMonedero(boolean monedero) {
+			this.monedero = monedero;
+		}
+
+		public int getCuentaBan() {
+			return cuentaBan;
+		}
+
+		public void setCuentaBan(int cuentaBan) {
+			this.cuentaBan = cuentaBan;
+		}
+
+		public int getPin() {
+			return pin;
+		}
+
+		public void setPin(int pin) {
+			this.pin = pin;
+		}
+
+		public int getCv2() {
+			return cv2;
+		}
+
+		public void setCv2(int cv2) {
+			this.cv2 = cv2;
+		}
+
+		public int getNumTarjeta() {
+			return numTarjeta;
+		}
+
+		public void setNumTarjeta(int numTarjeta) {
+			this.numTarjeta = numTarjeta;
+		}
+
+		public String getEstado() {
+			return estado;
+		}
+
+		public void setEstado(String estado) {
+			this.estado = estado;
+		}
+}
