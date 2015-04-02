0e3a210f2536245d9a596ae6d08c5a3e654d59eb
clara <clara@clara-VAIO>
2015-04-02 20:07:37 +0200

    VERSION 1.0 (RECHAZOS)

--- gtu.g12/src/gtu/g12/EstampadoraServlet.java
+++ gtu.g12/src/gtu/g12/EstampadoraServlet.java
@@ -19,6 +19,7 @@ public class EstampadoraServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
 		SolicitudDAO dao = SolicitudDAOImpl.getInstance();
